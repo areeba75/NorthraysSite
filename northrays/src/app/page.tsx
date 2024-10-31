cb341fa6766f3d71bc8bf9354d8ddf1642a00406
areeba75 <areebaars1@gmail.com>
2024-10-31 12:52:24 +0500

    First commit of Northrays

---
+++ northrays/src/app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Header from './Home/page';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import Portfolio from './Home/Portfolio';
+import Academy from './academy/page';
+AOS.init();
+
+
+
+export default function Home() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+    });
+  }, []);
+
+  return (
+    <>
+      <Header />
+      
+    </>
+  );
+}
